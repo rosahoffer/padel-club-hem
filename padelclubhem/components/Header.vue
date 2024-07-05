5b489c2417fdf651e8cbdadf1c7e68532eebf3d5
rosahoffer <rosahoffer@live.nl>
2024-07-05 20:26:04 +0200

    push

--- padelclubhem/components/Header.vue
+++ padelclubhem/components/Header.vue
@@ -1,285 +1,270 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import gsap from "gsap";
+
+const navToggle = ref(false);
+
+const toggleMenu = () => {
+  navToggle.value = !navToggle.value;
+  const navMenu = document.getElementById("navMenu");
+  const logo = document.getElementById("logo");
+  const bubbleContainer = document.querySelector(".bubble-container");
+
+  if (navToggle.value) {
+    gsap.to(navMenu, {
+      duration: 0.3,
+      height: "100vh",
+      opacity: 1,
+      display: "flex",
+      ease: "power2.inOut",
+    });
+    gsap.to(logo, { duration: 0.3, filter: "brightness(0) invert(1)" });
+    bubbleContainer.style.display = "block";
+  } else {
+    gsap.to(navMenu, {
+      duration: 0.3,
+      height: "0",
+      opacity: 0,
+      display: "none",
+      ease: "power2.inOut",
+    });
+    gsap.to(logo, { duration: 0.3, filter: "brightness(1) invert(0)" });
+    bubbleContainer.style.display = "none";
+  }
+};
+</script>
 
 <template>
-    <main>
-        <nav class="navbar">
-            <div>
-                <nuxt-link href="/" class="nav__link logo"><img src="assets/images/padelclubhem-logo.png" alt="Logo Padel Club Hem"></nuxt-link>
-            </div>
+  <main>
+    <div class="navbar-container">
+      <nav class="navbar">
+        <div class="logo-container">
+          <nuxt-link href="/" class="nav__link logo" id="logo">
+            <img
+              src="assets/images/padelclubhem-logo.png"
+              alt="Logo Padel Club Hem"
+            />
+          </nuxt-link>
+        </div>
 
-            <input type="checkbox" id="nav" class="hidden" />
-            <label for="nav" class="nav__open"><i></i><i></i><i></i></label>
-            <div class="nav">
-                <ul class="nav__items">
-                    <li class="nav__item">
-                        <nuxt-link href="/" class="nav__link">Sportaanbod</nuxt-link>
-                    </li>
-                    <li class="nav__item">
-                        <nuxt-link href="/" class="nav__link">Toernooien</nuxt-link>
-                    </li>
-                    <li class="nav__item">
-                        <nuxt-link href="/" class="nav__link">Lessen</nuxt-link>
-                    </li>
-                    <li class="nav__item">
-                        <nuxt-link href="/" class="nav__link">Business</nuxt-link>
-                    </li>
-                    <li class="nav__item">
-                        <nuxt-link href="/sportaanbod" class="nav__link">Contact</nuxt-link>
-                    </li>
-                </ul>
-            </div>
+        <div class="book-button-container">
+          <nuxt-link href="/" class="book-button" role="link"
+            >🏓 Boek een baan!</nuxt-link
+          >
+        </div>
 
-            <div>
-                <nuxt-link href="/" class="book-button" role="link">🏓 Boek een baan!</nuxt-link>
-            </div>
-        </nav>
-    </main>
+        <!-- Checkbox for the menu -->
+        <input type="checkbox" id="navToggle" class="hidden" />
+        <label for="navToggle" class="nav__open">
+          <i></i>
+          <i></i>
+        </label>
+
+        <!-- The menu itself -->
+        <div class="nav" id="navMenu">
+          <div class="bubble-container">
+            <div id="bubble"></div>
+          </div>
+          <ul class="nav__items">
+            <li class="nav__item">
+              <nuxt-link href="/sportaanbod" class="nav__link"
+                >Sportaanbod</nuxt-link
+              >
+            </li>
+            <li class="nav__item">
+              <nuxt-link href="/" class="nav__link">Toernooien</nuxt-link>
+            </li>
+            <li class="nav__item">
+              <nuxt-link href="/" class="nav__link">Lessen</nuxt-link>
+            </li>
+            <li class="nav__item">
+              <nuxt-link href="/" class="nav__link">Business</nuxt-link>
+            </li>
+            <li class="nav__item">
+              <nuxt-link href="/" class="nav__link">Contact</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </main>
 </template>
 
 <style scoped>
 :root {
-    /* COLORS */
-
-    --primary-color: #cbfd3c;
-    --secondary-color: #ffeae3;
-    --tertiary-color: #414141;
-    --background-color: #262626;
-    --navbar-bg-color: #4b4b4b;
+  /* COLORS */
+  --primary-color: #cbfd3c;
+  /* Groen */
+  --secondary-color: #ffeae3;
+  --tertiary-color: #414141;
+  --background-color: #262626;
+  /* Voor logo */
+  --navbar-bg-color: #4b4b4b;
 }
 
-main {
-    position: relative;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    border-radius: 15px;
-}
-
-.logo img {
-    width: 5rem;
+.navbar-container {
+  margin: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 }
 
 .navbar {
-    position: fixed;
-    display: flex;
-    align-items: center;
-    top: 1rem;
-    width: 95%;
-    padding: 1rem 1.5rem;
-    background-color: red;
-    border-radius: 15px;
-    z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: var(--navbar-bg-color); /* Adjust as per your design */
+  border-radius: 15px;
 }
 
-.book-button {
-    width: 10rem;
+.logo-container img {
+  height: 50px;
+  transition: filter 0.3s;
 }
 
 .hidden {
-    display: none;
+  display: none;
+}
+
+.nav__open {
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+.nav__open i {
+  background: var(--primary-color);
+  width: 25px;
+  height: 3px;
+  margin: 4px 0;
+  transition: all 0.3s;
+}
+
+.nav {
+  display: none;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: var(--primary-color);
+  z-index: 5;
+  opacity: 0;
+  overflow: hidden;
+  transition: all 0.3s;
+  padding: 1rem;
+  border-radius: 10px;
 }
 
 .nav__items {
-    width: 100%;
-    list-style: none;
-    background-color: var(--navbar-bg-color);
-    border-radius: var(--border-radius);
+  list-style: none;
+  padding: 5rem 0;
+  text-align: left;
+}
+
+.nav__item {
+  margin: 10px 0;
 }
 
 .nav__link {
-    color: var(--shades);
-    text-decoration: none;
+  color: var(--background-color);
+  text-decoration: none;
+  padding: 10px 20px;
+  display: block;
+  transition: background-color 0.3s;
 }
 
-.brand {
-    margin: 0;
-    font-size: 1.45em;
+.nav__link:hover {
+  background-color: #575757;
+  border-radius: 5px;
 }
 
-.brand a {
-    padding: 10px 15px;
-    text-align: center;
-    display: block;
+.hidden:checked + .nav__open + .nav {
+  position: absolute;
+  display: flex;
+  opacity: 0.5;
+  top: 0;
 }
 
-.logo {
-    display: inline-block;
-    padding-top: 0.3125rem;
-    padding-bottom: 0.3125rem;
-    margin-right: 1rem;
-    line-height: inherit;
+.hidden:checked + .nav__open i:nth-child(1) {
+  transform: translateY(10px) rotate(45deg);
 }
 
-.logo::before {
-    display: inline-block;
-    vertical-align: top;
+.hidden:checked + .nav__open i:nth-child(2) {
+  transform: translateY(0px) rotate(-45deg);
 }
 
-.nav__items {
-    margin-top: 5px;
+.bubble-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: none; /* Initially hidden */
 }
 
-.brand .nav__link,
-.nav__items .nav__link {
-    padding: 10px 15px;
-    text-align: center;
-    display: block;
+.nav.open .bubble-container {
+  display: block; /* Show bubble when menu open */
 }
 
-.nav__items .nav__link {
-    color: var(--shades);
-    font-size: 0.99rem;
+#bubble {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 200vw; /* Initial large circle */
+  height: 200vh;
+  background-color: var(--primary-color);
+  border-radius: 50%; /* Circle */
+  transition: all 0.3s ease;
+  z-index: -2;
 }
 
-.nav__items .nav__link:hover {
-    color: var(--secondary);
-}
+@media (min-width: 768px) {
+  .nav__open {
+    display: none; /* Hide hamburger icon on larger screens */
+  }
 
-@media (min-width: 800px) {
-
-    .navbar,
-    .nav__items {
-        display: flex;
-    }
-
-    .navbar {
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .navbar {
-        flex-direction: row;
-        justify-content: space-between;
-    }
-}
-
-@media screen and (max-width: 799px) {
-    .nav {
-        position: fixed;
-        top: 0;
-        left: -100%;
-        height: 100%;
-        width: 100%;
-        z-index: -1;
-        background-color: var(--primary-color);
-        opacity: 0;
-        transition: all 0.3s ease;
-    }
-
-    .nav .nav__items {
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 100%;
-        padding-inline-start: 0;
-    }
-
-    .nav .nav__items .nav__item {
-        display: block;
-        float: none;
-        width: 100%;
-        text-align: right;
-        margin-bottom: 10px;
-    }
-
-    .nav .nav__items .nav__item:nth-child(1) a {
-        transition-delay: 0.2s;
-    }
-
-    .nav .nav__items .nav__item:nth-child(2) a {
-        transition-delay: 0.3s;
-    }
-
-    .nav .nav__items .nav__item:nth-child(3) a {
-        transition-delay: 0.4s;
-    }
-
-    .nav .nav__items .nav__item:nth-child(4) a {
-        transition-delay: 0.5s;
-    }
-
-    .nav .nav__items .nav__item:not(:first-child) {
-        margin-left: 0;
-    }
-
-    .nav .nav__items .nav__item .nav__link {
-        padding: 10px 25px;
-        opacity: 0;
-        color: var(--shades);
-        font-size: 24px;
-        font-weight: 600;
-        transform: translateY(-20px);
-        transition: all 0.3s ease;
-    }
-
-    .nav .nav__items .nav__item .nav__link:hover {
-        color: pink;
-    }
-
-    .nav__open {
-        position: absolute;
-        right: 10px;
-        top: 16px;
-        display: block;
-        width: 48px;
-        height: 48px;
-        cursor: pointer;
-        z-index: 9999;
-        border-radius: 50%;
-    }
-
-    .nav__open i {
-        display: block;
-        width: 20px;
-        height: 2px;
-        background-color: var(--primary-color);
-        border-radius: 2px;
-        margin-left: 14px;
-    }
-
-    .nav__open i:nth-child(1) {
-        margin-top: 16px;
-    }
-
-    .nav__open i:nth-child(2) {
-        margin-top: 4px;
-        opacity: 1;
-    }
-
-    .nav__open i:nth-child(3) {
-        margin-top: 4px;
-    }
-}
-
-#nav:checked+.nav__open {
-    transform: rotate(45deg);
-}
-
-#nav:checked+.nav__open i {
-    background-color: var(--background-color);
-    transition: transform 0.2s ease;
-}
-
-#nav:checked+.nav__open i:nth-child(1) {
-    transform: translateY(6px) rotate(180deg);
-}
-
-#nav:checked+.nav__open i:nth-child(2) {
-    opacity: 0;
-}
-
-#nav:checked+.nav__open i:nth-child(3) {
-    transform: translateY(-6px) rotate(90deg);
-}
-
-#nav:checked~.nav {
-    transform: translateX(100%);
+  .nav {
+    display: flex; /* Show navbar on larger screens */
+    position: static;
+    width: auto;
+    background-color: transparent;
     opacity: 1;
-    z-index: 9990;
-}
+    padding: 0;
+    border-radius: 0;
+    flex-direction: row;
+    align-items: center;
+  }
 
-#nav:checked~.nav ul li a {
-    opacity: 1;
-    transform: translateY(0);
+  .nav__items {
+    margin-top: 0;
+    flex-direction: row;
+  }
+
+  .nav__item {
+    margin: 0 10px;
+  }
+
+  .book-button-container {
+    display: block;
+  }
+
+  .logo-container {
+    order: 1;
+  }
+
+  .nav {
+    order: 2;
+    justify-content: center;
+  }
+
+  .book-button-container {
+    order: 3;
+  }
 }
 </style>
