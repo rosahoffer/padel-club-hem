da2fb562042636407bbae9dba42b08b10115e90c
rosahoffer <rosahoffer@live.nl>
2024-07-14 12:20:30 +0200

    push

--- padelclubhem/pages/homepage.vue
+++ padelclubhem/pages/homepage.vue
@@ -19,7 +19,22 @@ import ScrollSeparator from '~/components/molecules/ScrollSeparator.vue';
                 sporten, sportwedstrijden te kijken of wellicht om een drankje te doen met vrienden of kennissen. Qua
                 sportfaciliteiten is er naast padel ook tennis, padel pingpong en een shuffle board. De bar is zoveel
                 als mogelijk geopend en er zal altijd sport worden uitgezonden op één van de schermen.</p>
-            <nuxt-link href="/" class="secondary-button">Bekijk ons sportaanbod</nuxt-link>
+            <nuxt-link href="#" class="secondary-button subtitle-medium">
+                Bekijk ons sportaanbod
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M5.28239 0.452916L14.7293 0.3712C15.309 0.366184 15.783 0.832064 15.788 1.41177L15.8697 10.8586C15.8747 11.4383 15.4088 11.9123 14.8291 11.9174C14.2494 11.9224 13.7754 11.4565 13.7704 10.8768L13.7106 3.96401L2.24266 15.6321C1.83629 16.0455 1.17169 16.0513 0.758226 15.6449C0.344766 15.2386 0.339017 14.574 0.745385 14.1605L12.2133 2.49242L5.30055 2.55222C4.72085 2.55723 4.24684 2.09135 4.24182 1.51165C4.23681 0.93194 4.70269 0.457931 5.28239 0.452916Z"
+                        fill="#414141" />
+                </svg>
+            </nuxt-link>
+            <nuxt-link href="#" class="secondary-button subtitle-medium">
+                Benieuwd naar onze locatie?
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M5.28239 0.452916L14.7293 0.3712C15.309 0.366184 15.783 0.832064 15.788 1.41177L15.8697 10.8586C15.8747 11.4383 15.4088 11.9123 14.8291 11.9174C14.2494 11.9224 13.7754 11.4565 13.7704 10.8768L13.7106 3.96401L2.24266 15.6321C1.83629 16.0455 1.17169 16.0513 0.758226 15.6449C0.344766 15.2386 0.339017 14.574 0.745385 14.1605L12.2133 2.49242L5.30055 2.55222C4.72085 2.55723 4.24684 2.09135 4.24182 1.51165C4.23681 0.93194 4.70269 0.457931 5.28239 0.452916Z"
+                        fill="#414141" />
+                </svg>
+            </nuxt-link>
         </section>
     </main>
 </template>
@@ -54,7 +69,7 @@ import ScrollSeparator from '~/components/molecules/ScrollSeparator.vue';
     gap: 3rem;
 }
 
-.intro-container p{
+.intro-container p {
     color: var(--secondary-color);
     padding: 0 1.5rem;
 }
@@ -70,14 +85,20 @@ import ScrollSeparator from '~/components/molecules/ScrollSeparator.vue';
     display: flex;
     flex-direction: column;
     background-color: var(--secondary-color);
-    padding: 0 1.5rem;
+    padding: 0 1.5rem 3rem 1.5rem;
 }
 
-.container-two > h2, p {
+.container-two>h2,
+p {
     color: var(--tertiary-color);
 }
 
 .container-two p {
-    margin-top: 2rem;
+    margin: 2rem 0;
 }
+
+.secondary-button:last-child{
+    border-top: none;
+}
+
 </style>
--- padelclubhem/pages/index.vue
+++ padelclubhem/pages/index.vue
@@ -1,5 +1,31 @@
 <script setup></script>
 
 <template>
-  <ComingSoon/>
+
+<head>
+  <title>Padel Club Hem - Boek een Padel- of Tennisbaan in West-Friesland</title>
+  <meta name="description" content="Ontdek de gezelligste indoor sportclub in West-Friesland. Boek vandaag nog een padelbaan of tennisbaan bij Padel Club Hem en beleef sport op topniveau.">
+  <meta name="keywords" content="padellen, tennissen, padelbaan boeken, tennisbaan boeken, padelbaan huren, tennisbaan huren, padellen West-Friesland, tennissen West-Friesland, Padel Club Hem">
+
+  <!-- Facebook-pagina -->
+  <meta property="og:url" content="https://www.facebook.com/profile.php?id=61560566112040" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Padel Club Hem" />
+  <meta property="og:description" content="Ontdek de gezelligste indoor sportclub in West-Friesland. Boek vandaag nog een padelbaan of tennisbaan bij Padel Club Hem en beleef sport op topniveau." />
+  <meta property="og:image" content="https://www.padelclubhem.nl/_nuxt/padelclubhem-logo.D4sbnd6b.png" />
+
+  <!-- Adres -->
+  <meta name="address" content="Torenweg 44, 1607 CP Hem, Nederland" />
+  <meta name="geo.region" content="NL-NH">
+  <meta name="geo.placename" content="Hem">
+  <meta property="business:contact_data:street_address" content="Torenweg 44">
+  <meta property="business:contact_data:postal_code" content="1607 CP">
+  <meta property="business:contact_data:locality" content="Hem">
+  <meta property="business:contact_data:country_name" content="Nederland">
+
+  <!-- Openingstijden -->
+  <meta property="openingHours" content="ma-vr 07:00 t/m 24:00 uur, za-zo 8:00 t/m 23:00 uur" />
+</head>
+
+  <ComingSoon />
 </template>
