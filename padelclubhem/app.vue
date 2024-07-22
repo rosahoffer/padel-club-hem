b606431fb15d170e551257db7613c99906723088
rosahoffer <rosahoffer@live.nl>
2024-07-22 12:57:00 +0200

    push

--- padelclubhem/app.vue
+++ padelclubhem/app.vue
@@ -18,7 +18,7 @@ useSeoMeta({
   meta: [
     { name: 'description', content: 'Ontdek de gezelligste indoor sportclub in West-Friesland. Boek vandaag nog een padelbaan of tennisbaan bij Padel Club Hem en beleef sport op topniveau.' },
     { name: 'keywords', content: 'padellen, tennissen, padelbaan boeken, tennisbaan boeken, padelbaan huren, tennisbaan huren, padellen West-Friesland, tennissen West-Friesland, Padel Club Hem, Padelclub Hem' },
-    { property: 'og:url', content: 'https://www.facebook.com/profile.php?id=61560566112040' },
+    { property: 'og:url', content: 'https://www.padelclubhem.nl' },
     { property: 'og:type', content: 'website' },
     { property: 'og:title', content: 'Padel Club Hem' },
     { property: 'og:description', content: 'Ontdek de gezelligste indoor sportclub in West-Friesland. Boek vandaag nog een padelbaan of tennisbaan bij Padel Club Hem en beleef sport op topniveau.' },
--- padelclubhem/nuxt.config.ts
+++ padelclubhem/nuxt.config.ts
@@ -19,7 +19,10 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       htmlAttrs: {
         lang: 'nl'
-      }
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     }
   }
 });
