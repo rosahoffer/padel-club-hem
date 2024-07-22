02aa8f684c86e9c83ce5805613e263bb781d9aba
rosahoffer <rosahoffer@live.nl>
2024-07-22 15:29:07 +0200

    push

--- padelclubhem/components/sportaanbod/SectionOne.vue
+++ padelclubhem/components/sportaanbod/SectionOne.vue
@@ -14,7 +14,7 @@
                 <MoleculesBallSeparator />
             </div>
         </div>
-        <section>
+        <section class="section-image-wrapper">
             <div class="image-wrapper">
                 <div class="image-overlay">
                     <div class="image-container">
@@ -22,6 +22,11 @@
                             alt="Afbeelding man die aan het paddellen is" loading="lazy" width="100%" height="100%">
                     </div>
                     <div class="green-separator"></div>
+                </div>
+            </div>
+            <div class="scroll-down-container-wrapper">
+                <div class="scroll-down-container">
+                    <AtomsRacketScrollDown />
                 </div>
             </div>
         </section>
@@ -49,7 +54,7 @@
 .ball-separator-wrapper {
     overflow: hidden;
     width: 100%;
-    height: 7rem;
+    height: 8rem;
 }
 
 .ball-separator {
@@ -60,17 +65,85 @@
     height: 100%;
 }
 
+.image-overlay {
+    width: 100%;
+    height: 20rem;
+}
+
+.section-image-wrapper {
+    position: relative;
+}
+
+.scroll-down-container-wrapper {
+    position: absolute;
+    top: -2rem;
+    right: 3rem;
+    background-color: transparent;
+    z-index: 10;
+}
+
 @media (min-width: 40rem) {
     .intro-content {
         gap: 3rem;
         padding: 11rem 3rem 3rem 3rem;
     }
+
+    .image-overlay {
+        width: 100%;
+        height: 30rem;
+    }
 }
 
 @media (min-width: 60rem) {
     .wrapper {
+        position: relative;
         display: flex;
-        gap: 3rem;
+        justify-content: space-between;
+        height: 100vh;
+        padding-top: 3rem;
+    }
+
+    .intro-content>p {
+        max-width: 35rem;
+    }
+
+    .ball-separator-wrapper {
+        position: absolute;
+        bottom: 0;
+        height: 8rem;
+    }
+
+    .section-image-wrapper {
+        width: 100%;
+        height: auto;
+        margin-top: auto;
+    }
+
+    .image-overlay {
+        width: 100%;
+    }
+
+    .scroll-down-container-wrapper {
+        top: -3rem;
+        right: 5rem;
+    }
+}
+
+@media (min-width: 80rem) {
+
+    .section-image-wrapper {
+        width: 75%;
+        height: auto;
+        margin-top: auto;
+    }
+
+    .image-overlay {
+        width: 100%;
+    }
+
+    .scroll-down-container-wrapper {
+        top: -4rem;
+        right: 7rem;
     }
 }
 </style>
