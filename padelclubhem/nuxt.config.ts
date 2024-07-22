971104459bd19ae6b15ccd2f6fcf41b17d29638f
rosahoffer <rosahoffer@live.nl>
2024-07-22 12:25:30 +0200

    push

--- padelclubhem/nuxt.config.ts
+++ padelclubhem/nuxt.config.ts
@@ -1,17 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   css: [
-    'assets/styles/main.css'
+    '@/assets/styles/main.css' 
   ],
 
-  compatibilityDate: '2024-07-05',
-  modules: ["nuxt-marquee", "@nuxtjs/seo", "@nuxtjs/html-validator"],
+  compatibilityDate: '2024-07-05', 
+
+  modules: [
+    'nuxt-marquee',
+    '@nuxtjs/seo'
+  ],
+
   components: true,
 
   app: {
-    pageTransition: { name: 'page', mode: 'in-out'},
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
